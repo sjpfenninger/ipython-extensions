f69cde162825e1b02db632da65fc4d68136ffa6d
Stefan Pfenninger <stefan@pfenninger.org>
2014-05-10 17:44:43 +0100

    Generalized to switch between two CSS files

--- README.md
+++ README.md
@@ -1,22 +1,24 @@
-# Toggle light / dark mode
+# Toggle between two IPython notebook themes
+
+IPython notebook extension to toggle between two CSS themes, `theme_default.css` and `theme_alt.css`. If `theme_default.css` is not found, no CSS is loaded for the default setting, i.e. only the pre-installed default theme is used.
 
 Installation:
 
-    $ curl -L https://raw.github.com/sjpfenninger/ipython-dark/master/dark.js > $(ipython locate)/nbextensions/dark.js
-    $ curl -L https://raw.github.com/sjpfenninger/ipython-dark/master/dark.css > $(ipython locate)/nbextensions/dark.css
+    $ curl -L https://raw.github.com/sjpfenninger/ipython-theme-toggle/master/theme_toggle.js > $(ipython locate)/nbextensions/theme_toggle.js
+    $ curl -L https://raw.github.com/sjpfenninger/ipython-theme-toggle/master/theme_alt.css > $(ipython locate)/nbextensions/theme_alt.css
 
 Then, load it with the following code in your `custom.js`:
 
 ```javascript
-IPython.load_extensions('dark');
+IPython.load_extensions('theme_toggle');
 ```
 
-To turn on the dark mode by default, add the following after the loading code:
+To load the alternative theme (`theme_alt.css`) by default, add the following after the loading code:
 
 ```javascript
-// Set dark mode by default
-require(["nbextensions/dark"], function (dark) {
-    $([IPython.events]).on("notebook_loaded.Notebook", dark.dark_toggle);
+// Always load alternative theme
+require(["nbextensions/theme_toggle"], function (theme_toggle) {
+    $([IPython.events]).on("notebook_loaded.Notebook", theme_toggle.theme_toggle);
 });
 ```
 
@@ -24,7 +26,7 @@ To enable a ``t`` as a keyboard shortcut, add the following after the loading co
 
 ```javascript
 // Enable keyboard shortcut
-require(["nbextensions/dark"], function (dark) {
-    $([IPython.events]).on("notebook_loaded.Notebook", dark.dark_shortcut);
+require(["nbextensions/theme_toggle"], function (theme_toggle) {
+    $([IPython.events]).on("notebook_loaded.Notebook", theme_toggle.theme_toggle_shortcut);
 });
 ```
---
+++ theme_toggle.js
@@ -0,0 +1,93 @@
+/*
+
+*************************
+Toggle between two themes
+*************************
+
+Add this file and neighboring dark.css to $(ipython locate)/nbextensions/
+
+Adapted from the toc nbextension, https://github.com/minrk/ipython_extensions
+
+*/
+
+define(["require"], function (require) {
+  "use strict";
+
+  var themes = {def: "./theme_default.css", alt: "./theme_alt.css"};
+
+  var theme_toggle_button = function () {
+    if (!IPython.toolbar) {
+      $([IPython.events]).on("app_initialized.NotebookApp", theme_toggle_button);
+      return;
+    }
+    if ($("#theme-toggle-button").length === 0) {
+      IPython.toolbar.add_buttons_group([
+        {
+          'label'   : 'Toggle Bright/Dark Mode',
+          'icon'    : 'icon-adjust',
+          'callback': theme_toggle,
+          'id'      : 'theme-toggle-button'
+        },
+      ]);
+    }
+  };
+
+  var theme_toggle_shortcut = function () {
+    if (!IPython.toolbar) {
+      $([IPython.events]).on("app_initialized.NotebookApp", theme_toggle_shortcut);
+      return;
+    }
+    IPython.keyboard_manager.command_shortcuts.add_shortcut('t', {
+      help : 'toggle theme',
+      help_index : 't',
+      handler : function (event) {
+        theme_toggle();
+        return false;
+      }}
+    );
+  };
+
+  var load_css = function (theme) {
+    var link = document.createElement("link");
+    link.type = "text/css";
+    link.rel = "stylesheet";
+    link.href = require.toUrl(themes[theme]);
+    link.id = theme + "-css";
+    document.getElementsByTagName("head")[0].appendChild(link);
+  };
+
+  var unload_css = function (theme) {
+    var css = document.getElementById(theme + "-css");
+    css.parentNode.removeChild(css);
+  };
+
+  var is_active = function (theme) {
+    if ($("#" + theme + "-css").length > 0) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  var theme_toggle = function () {
+    if (is_active("alt")) {
+      unload_css("alt");
+      load_css("def");
+    } else {
+      unload_css("def");
+      load_css("alt");
+    }
+  };
+
+  var load_ipython_extension = function () {
+    theme_toggle_button();
+    $([IPython.events]).on("app_initialized.NotebookApp", load_css("def"));
+  };
+
+  return {
+    load_ipython_extension : load_ipython_extension,
+    theme_toggle : theme_toggle,
+    theme_toggle_shortcut : theme_toggle_shortcut
+  };
+
+});
