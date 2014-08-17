e1b3f934dcea8fb3c87265dc7a26a79172f878b3
Stefan Pfenninger <stefan@pfenninger.org>
2014-08-17 12:34:09 +0100

    Added notify.js

--- README.md
+++ README.md
@@ -34,3 +34,39 @@ require(["nbextensions/theme_toggle"], function (theme_toggle) {
     $([IPython.events]).on("notebook_loaded.Notebook", theme_toggle.theme_toggle_shortcut);
 });
 ```
+
+## Notebook web notifications
+
+Installation:
+
+    $ curl -L https://raw.github.com/sjpfenninger/ipython-extensions/master/nbextensions/notify.js > $(ipython locate)/nbextensions/notify.js
+
+Then, load it with the following code in your `custom.js`:
+
+```javascript
+IPython.load_extensions('notify');
+```
+
+## License
+
+The MIT License (MIT)
+
+Copyright (c) 2014 Stefan Pfenninger
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ nbextensions/notify.js
@@ -0,0 +1,163 @@
+/*
+
+*************************
+Display Web Notifications
+*************************
+
+Add this file to $(ipython locate)/nbextensions/
+
+*/
+
+define(["require"], function (require) {
+  "use strict";
+
+  var current_time = function() {
+    return new Date().getTime() / 1000;
+  };
+
+  var start_time = current_time();
+  var min_time = 0;
+  var enabled = false;
+  var first_start = true;
+  var busy_kernel = true;
+
+  var add_permissions_list = function () {
+    var ipython_toolbar = $('#maintoolbar-container');
+    var label = $('<span/>').addClass("navbar-text permissions-list").text('Notify:');
+    var select = $('<select/>')
+                  .attr('id', 'permissions-select')
+                  .attr('class', 'permissions-list')
+                  .append($('<option/>')
+                  .attr('value', 'Disabled')
+                  .text('Disabled'));
+    ipython_toolbar.append(label).append(select);
+    select.change(function() {
+      var val = $(this).val();
+      if (val == 'Disabled') {
+        enabled = false;
+      } else {
+        enabled = true;
+        min_time = val;
+      }
+      save_state();
+    });
+    // Add options in addition to the default, 'Disabled'
+    // Options give the minimum kernel busy time in seconds after which a notification is displayed
+    var presets = [0, 5, 10, 30];
+    for (var i=0; i<presets.length; i++) {
+      var name = presets[i];
+      select.append($('<option/>').attr('value', name).text(name));
+    }
+  };
+
+  var add_permissions_button = function () {
+    if ($("#permissions-button").length === 0) {
+      IPython.toolbar.add_buttons_group([
+        {
+          'label'   : 'Grant Notification Permissions',
+          'icon'    : 'icon-ok',
+          'callback': ask_permission,
+          'id'      : 'permissions-button'
+        },
+      ]);
+    }
+  };
+
+  var ensure_permission = function () {
+    $([IPython.events]).on("app_initialized.NotebookApp", function () {
+      ask_permission();  // Asks for permission on notebook load, doesn't work in Chrome
+      // If don't have permission now, add a button to the toolbar to let user request permission
+      if (Notification && Notification.permission !== "granted") {
+        add_permissions_button();
+        add_permissions_list();
+        $(".permissions-list").hide();
+      } else if (Notification && Notification.permission === "granted") {
+        add_permissions_list();
+      }
+    });
+  };
+
+  var ask_permission = function () {
+    if (Notification && Notification.permission !== "granted") {
+      Notification.requestPermission(function (status) {
+        if (Notification.permission !== status) {
+          Notification.permission = status;
+        }
+        // Wait for permission to be granted, then remove the permissions-button and show permissions-list
+        if (Notification && Notification.permission === "granted" && $("#permissions-button").length > 0) {
+          $("#permissions-button").remove();
+          $(".permissions-list").show();
+        }
+      });
+    }
+  };
+
+
+  var notify = function () {
+    var elapsed_time = current_time() - start_time;
+    if (enabled && !first_start && !busy_kernel && elapsed_time >= min_time) {
+      var n = new Notification(IPython.notebook.notebook_name, {body: "Kernel is now idle\n(" + Math.round(elapsed_time) + " secs)"});
+    }
+    if (first_start) {
+      first_start = false;
+    }
+  };
+
+  var load_state = function () {
+    if (!IPython.notebook) return;
+
+    if ("notify_time" in IPython.notebook.metadata) {
+      min_time = IPython.notebook.metadata.notify_time;
+      enabled = true;
+    }
+  };
+
+  var save_state = function () {
+    if (enabled) {
+      IPython.notebook.metadata.notify_time = min_time;
+    } else {
+      delete IPython.notebook.metadata.notify_time;
+    }
+  };
+
+  var restore_state = function () {
+    $([IPython.events]).on("notebook_loaded.Notebook", function () {
+      load_state();
+      // Only proceed if the permissions selector is being shown
+      if ($("#permissions-select").length > 0) {
+        if (!enabled) {
+          $("#permissions-select").val("Disabled");
+        } else {
+          $("#permissions-select").val(min_time);
+        }
+      }
+    });
+  };
+
+  var setup_notifier = function () {
+    $([IPython.events]).on('status_restarting.Kernel',function () {
+      first_start = true;  // reset first_start status when restarting the kernel
+    });
+
+    $([IPython.events]).on('status_busy.Kernel',function () {
+      busy_kernel = true;
+      start_time = current_time();  // reset the timer
+    });
+
+    $([IPython.events]).on('status_idle.Kernel',function () {
+      busy_kernel = false;  // Used to make sure that kernel doesn't go busy again within the timeout set below.
+      setTimeout(notify, 500);
+    });
+  };
+
+  var load_ipython_extension = function () {
+    ensure_permission();
+    setup_notifier();
+    restore_state();
+  };
+
+  return {
+    load_ipython_extension : load_ipython_extension
+  };
+
+});
